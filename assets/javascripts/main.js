5bcb08315a029cfeedef442909a1cd662a728b67
Mauno Vähä <vahamau@gmail.com>
2015-08-01 22:31:55 +0300

    Adds rest of the main functionalities

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -5,7 +5,8 @@
  * Image overlay
  * 
  **********************************************************************/
-var ImageOverlay = function() {
+var ImageOverlay = function(main) {
+  this.main = main;
   this.init();
 };
 ImageOverlay.prototype = {
@@ -71,8 +72,134 @@ ImageOverlay.prototype = {
  * Contact section
  * 
  **********************************************************************/
-var ContactForm = function() {};
+var ContactForm = function(main) {
+  this.main = main;
+  this.init();
+};
 ContactForm.prototype = {
+  /**
+   * Initializes everything
+   */
+  init: function() {
+    var self = this;
+
+    $('#send-feedback').on('click', function(e) {
+      self.send(e);
+    });
+  },
+
+  /**
+   * Handles sending of email, and required steps
+   */
+  send: function(e) {
+    e.preventDefault();
+
+    if(!$('#feedback-overlay').hasClass('feedback-overlay-open')) {
+      var isValid = this.isValid();
+
+      if(isValid) {
+        this.email();
+      } else {
+        this.setState('validation-error');
+      }
+    }
+  },
+
+  /**
+   * Clears all form inputs, called when feedback is sent
+   */
+  clearFormInputs: function() {
+    $("#name").val('');
+    $("#phone").val('');
+    $("#message").val('');
+    $("#email").val('');
+  },
+
+  /**
+   * Sends the actual email
+   */
+  email: function() {
+    var self = this;
+
+    self.setState('loading');
+
+    $.ajax({
+      url: "https://ylivieskankivimiehet.herokuapp.com/feedback",
+      type: "POST",
+      data: self.userInput(),
+      cache: false,
+    }).done(function(data, statusText, xhr) {
+      var status = xhr.status;
+
+      if (status == 200) {
+        self.setState('success');
+        self.clearFormInputs();
+      } else {
+        self.setState('server-error');
+      }
+
+    }).fail(function() {
+      self.setState('server-error');
+    });
+  },
+
+  /**
+   * Sets loading state
+   */
+  setState: function(state) {
+
+    $('.success').addClass('hidden');
+    $('.validation-error').addClass('hidden');
+    $('.server-error').addClass('hidden');
+
+    switch(state) {
+      case 'loading':
+        $('body').addClass('overflow-hidden');
+        $('#feedback-overlay').addClass('feedback-overlay-open'); 
+        break;
+      case 'validation-error':
+        $('.validation-error').removeClass('hidden');
+        break;
+      case 'server-error':
+        $('.server-error').removeClass('hidden');
+        break;
+      case 'success':
+        $('.success').removeClass('hidden');
+        break;
+    } 
+
+    if (state !== 'loading') {
+      $('body').removeClass('overflow-hidden');
+      $('#feedback-overlay').removeClass('feedback-overlay-open'); 
+    }
+  },
+
+  /**
+   * Validates given input, nice and simple.
+   */
+  isValid: function() {
+    var userInput = this.userInput();
+
+    return userInput['name'].length        > 0 && 
+           userInput['phone'].length       > 0 && 
+           userInput['message'].length     > 0 && 
+           userInput['email'].length       > 0 &&
+           userInput['email'].indexOf('@') > -1;
+  },
+
+  /**
+   * 
+   */
+  userInput: function() {
+    return {
+      'name':    $.trim($('#name').val()),
+      'phone':   $.trim($('#phone').val()),
+      'message': $.trim($('#message').val()),
+      'email':   $.trim($('#email').val())
+    };
+  },
+
+
 };
 
 /***********************************************************************
@@ -81,8 +208,35 @@ ContactForm.prototype = {
  * 
  **********************************************************************/
 var Main = function() {
-  this.imageOverlay = new ImageOverlay();
-  this.contactForm  = new ContactForm();
+  this.imageOverlay = new ImageOverlay(this);
+  this.contactForm  = new ContactForm(this);
+
+  // Launch spinner
+  var opts = {
+    lines: 13, // The number of lines to draw
+    length: 28, // The length of each line
+    width: 14, // The line thickness
+    radius: 42, // The radius of the inner circle
+    scale: 1, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    color: '#000', // #rgb or #rrggbb or array of colors
+    opacity: 0.25, // Opacity of the lines
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    className: 'spinner', // The CSS class to assign to the spinner
+    top: '50%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    position: 'fixed' // Element positioning
+  };
+
+  var target = document.getElementById('feedback-overlay');
+  var spinner = new Spinner(opts).spin(target);
 
   // Handle to nav
   this.$nav = undefined;
@@ -118,7 +272,9 @@ Main.prototype = {
     };
     
     // Binds scrolling clicks to necessary elements
-    $('.scroll-to').on('click', this.scrollTo);
+    $('.scroll-to').on('click', function(e) {
+      self.scrollToClick(e, this);
+    });
 
     // Binds scrolling logic, so that proper nav element can be selected
     $(window).scroll(function() { 
@@ -127,16 +283,24 @@ Main.prototype = {
   },
 
   /**
+   * Binds scrolling clicks
+   */
+  scrollToClick: function(e, that) {
+    e.preventDefault();
+    this.scrollTo($('#' + $(that).data('scroll-to')));
+  },
+
+  /**
    * Scrolls to proper page section
    */
-  scrollTo: function(e) {
-    e.preventDefault();
-
-    var $target = $('#' + $(this).data('scroll-to'));
-
+  scrollTo: function(target, cb) {
     $('body, html').animate({
-      scrollTop: $target.offset().top
-    }, 900, 'easeInOutQuart', function() {});
+      scrollTop: target.offset().top
+    }, 900, 'easeInOutQuart', function() {
+      if (cb) { 
+        cb();
+      }
+    });
   },
 
   /**
